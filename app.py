e5c104875dd251a6921bff703a90747d4639f037
naomizoe <51715299+naomizoe@users.noreply.github.com>
2021-12-18 20:22:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import pandas as pd
 
 df = pd.read_csv("parameters_v2.csv", delimiter = ';')
 
